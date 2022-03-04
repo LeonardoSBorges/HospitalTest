766e79f3764b1ad0e2ef70100f7b9da568e33207
Leonardo Borges <ldssborges@hotmail.com>
2022-03-03 23:27:09 -0300

    Comentarios

--- Files/RegisterFile.cs
+++ Files/RegisterFile.cs
@@ -1,55 +1,55 @@
-﻿using HospitaTest.Entities;
-using System;
-using System.IO;
+﻿//using HospitaTest.Entities;
+//using System;
+//using System.IO;
 
-namespace HospitaTest.Files
-{
-    internal class RegisterFile
-    {
-        public Person person { get; set; }
+//namespace HospitaTest.Files
+//{
+//    internal class RegisterFile
+//    {
+//        public Person person { get; set; }
 
-        public RegisterFile()
-        {
-            person = GetRegister();
-        }
-        public void PushRegister(Person person)
-        {
-            Person personRegister;
-            string NameFile = @"C:\Users\LeonardoBorges\source\repos\ExerciciosAula\ListaDeExercicios\HospitaTest\SaveFiles\" + DateTime.Now.ToString("dd/MM/yyyy") + ".csv";
+//        public RegisterFile()
+//        {
+//            person = GetRegister();
+//        }
+//        public void PushRegister(Person person)
+//        {
+//            Person personRegister;
+//            string NameFile = @"C:\Users\LeonardoBorges\source\repos\ExerciciosAula\ListaDeExercicios\HospitaTest\SaveFiles\" + DateTime.Now.ToString("dd/MM/yyyy") + ".csv";
 
-            try
-            {
-                StreamWriter sw = new StreamWriter(NameFile);
-                while(())
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+//            try
+//            {
+//                StreamWriter sw = new StreamWriter(NameFile);
+//                while(())
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine(ex.Message);
+//            }
 
-        }
-        public Person GetRegister()
-        {
-            Person personRegister;
-            string NameFile = @"C:\Users\LeonardoBorges\source\repos\ExerciciosAula\ListaDeExercicios\HospitaTest\SaveFiles";
-            if (File.Exists(NameFile))
-            {
-                try
-                {
-                    StreamReader sr = new StreamReader(NameFile);
-                    string line;
-                    while ((line = sr.ReadLine()) != null)
-                    {
+//        }
+//        public Person GetRegister()
+//        {
+//            Person personRegister;
+//            string NameFile = @"C:\Users\LeonardoBorges\source\repos\ExerciciosAula\ListaDeExercicios\HospitaTest\SaveFiles";
+//            if (File.Exists(NameFile))
+//            {
+//                try
+//                {
+//                    StreamReader sr = new StreamReader(NameFile);
+//                    string line;
+//                    while ((line = sr.ReadLine()) != null)
+//                    {
 
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Type error: " + ex.Message);
-                }
-            }
+//                    }
+//                }
+//                catch (Exception ex)
+//                {
+//                    Console.WriteLine("Type error: " + ex.Message);
+//                }
+//            }
 
-            return
-        }
-    }
-}
+//            return
+//        }
+//    }
+//}
